ae4e7ce9d944a55424c6abc0c8924eff2dc7533b
나호연 <91938347+nahoyeon@users.noreply.github.com>
2022-08-22 17:59:12 +0900

    [Bronze V] Title: A-B, Time: 148 ms, Memory: 9460 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.js
@@ -0,0 +1,15 @@
+const readline = require('readline').createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+let input =[];
+
+readline.on('line', function(line) {
+    input = line.split(' ').map(el => parseInt(el));
+}).on('close', function() {
+    const a = parseInt(input[0]);
+    const b = parseInt(input[1]);
+    console.log(a-b);
+    process.exit();
+});
