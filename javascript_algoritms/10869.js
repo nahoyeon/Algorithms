7c6b3e5011c97d0b1f0c426a8dfcc4bb6ca3b0f4
nahoyeon <nahy0107@naver.com>
2022-08-23 17:27:30 +0900

    solve

---
+++ javascript_algoritms/10869.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().split(' ');
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(Math.floor(a/b));
+console.log(a%b);
