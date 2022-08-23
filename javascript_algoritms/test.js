a90fb3e2769451e1888ec4add962f4d36383aa36
nahoyeon <nahy0107@naver.com>
2022-08-23 17:54:28 +0900

    18108 solve

--- javascript_algoritms/test.js
+++ javascript_algoritms/test.js
@@ -1,1 +1,17 @@
-console.log("github test");
+const readline = require('readline').createInterface({
+    input : process.stdin,
+    output : process.stdout,
+});
+
+let input =[];
+
+readline.on('line', function(line) {
+  input = line.split(' ').map(el => parseInt(el));
+}).on('close', function() {
+
+    process.exit();
+});
+
+
+// const fs = require('fs');
+// const input = fs.readFileSync("./dev/stdin").toString().split(' ');
