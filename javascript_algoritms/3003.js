39a2c360512f8f70eca0e4a6eec59e099de07cd2
nahoyeon <nahy0107@naver.com>
2022-08-24 01:13:20 +0900

    solve

---
+++ javascript_algoritms/3003.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().split(' ').map(Number);
+
+const chess = [1, 1, 2, 2, 2, 8];
+
+let answer = input.map((el, index) => {
+    return chess[index] - el;
+})
+
+console.log(...answer);
--- javascript_algoritms/test.js
+++ javascript_algoritms/test.js
@@ -1,17 +1,17 @@
-const readline = require('readline').createInterface({
-    input : process.stdin,
-    output : process.stdout,
-});
+// const readline = require('readline').createInterface({
+//     input : process.stdin,
+//     output : process.stdout,
+// });
 
-let input =[];
+// let input =[];
 
-readline.on('line', function(line) {
-  input = line.split(' ').map(el => parseInt(el));
-}).on('close', function() {
+// readline.on('line', function(line) {
+//   input = line.split(' ').map(el => parseInt(el));
+// }).on('close', function() {
 
-    process.exit();
-});
+//     process.exit();
+// });
 
 
-// const fs = require('fs');
-// const input = fs.readFileSync("./dev/stdin").toString().split(' ');
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().split(' ');
