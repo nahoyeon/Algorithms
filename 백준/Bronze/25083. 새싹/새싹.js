3658018fe18cf0ff044165472b81ba4a1acdc1cd
나호연 <91938347+nahoyeon@users.noreply.github.com>
2022-08-24 17:01:20 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().split(' ');
+
+console.log(`         ,r'"7
+r\`-_   ,'  ,/
+ \\. ". L_r'
+   \`~\\/
+      |
+      |`);
