e8ef79e4ea8dc5be0e65a04ae0e44a0a1fc20e1a
나호연 <91938347+nahoyeon@users.noreply.github.com>
2022-08-25 16:02:15 +0900

    [Bronze III] Title: 오븐 시계, Time: 152 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -10,5 +10,11 @@ const loadingMinute = Number(input[1]);
 const finishHour = parseInt((nowHour * 60 + nowMinute + loadingMinute) / 60);
 const finishMinute = parseInt((nowHour * 60 + nowMinute + loadingMinute) % 60);
 
-console.log(finishHour >= 24 ? finishHour - 24 : finishHour, finishMinute);
+// console.log(finishHour >= 24 ? finishHour - 24 : finishHour, finishMinute);
 
+if(24 > finishHour){
+    console.log(`${finishHour} ${finishMinute}`);
+}
+else{
+    console.log(`${finishHour - 24} ${finishMinute}`)
+}
