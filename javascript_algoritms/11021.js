22d64d12be959b012b7e8899d13c087aa547ab92
nahoyeon <nahy0107@naver.com>
2022-08-27 23:38:04 +0900

    solve

---
+++ javascript_algoritms/11021.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
+
+for (let i = 1; i <= input[0]; i++) {
+    let numbers = input[i].split(' ');
+    
+    console.log(`Case #${i}: ${Number(numbers[0]) + Number(numbers[1])}`);
+}
