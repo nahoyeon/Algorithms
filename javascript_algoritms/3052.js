2d5586b03979512c415ab870a7afba392688523b
nahoyeon <nahy0107@naver.com>
2022-09-03 20:25:12 +0900

    solve

---
+++ javascript_algoritms/3052.js
@@ -0,0 +1,13 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(x => Number(x));
+let numArr = [];
+
+input.forEach(x => {
+const num = x % 42;
+
+if(numArr.indexOf(num) === -1) { 
+    numArr.push(num);             
+}
+});
+
+console.log(numArr.length);
+
