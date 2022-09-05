9aa2b656074c80cec4a6d7bf4cf468ff437eed8e
나호연 <91938347+nahoyeon@users.noreply.github.com>
2022-09-05 15:26:34 +0900

    [Bronze I] Title: 평균, Time: 120 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.js
+++ 백준/Bronze/1546. 평균/평균.js
@@ -3,7 +3,7 @@ const input = fs.readFileSync("./dev/stdin").toString().trim().split('\n');
 
 const count = input[0]; // 3
 const arr = input[1].split(' ');
-const max = Math.max.apply(null, arr);;
+const max = Math.max(...arr);
 let sum = 0;
 
 for (let i = 0; i < count; i++) {
