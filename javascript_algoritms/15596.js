af8043ecdbaa76fc58d5b587185a37a5ba3c6059
nahoyeon <nahy0107@naver.com>
2022-09-07 08:08:21 +0900

    Create 15596.js
    solve

---
+++ javascript_algoritms/15596.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync("./dev/stdin").toString().trim().split(' ');
+
+function func(arr){
+    let sum = 0;
+    for(let i = 0; i < n; i++){
+        sum += arr[i]
+    }
+}
+func();
